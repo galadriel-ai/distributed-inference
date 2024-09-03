197a2ddf6bb6cbcbce9349ea6a184f34f5cdcd60
kristjanpeterson <kristjan@nftport.xyz>
2024-09-03 12:01:26 +0300

    Benchmark handling: - Check if node benchmark is too low when connecting - Expect "Model" header with websocket connection - Fix benchmark query to check model as well

--- distributedinference/service/node/websocket_service.py
+++ distributedinference/service/node/websocket_service.py
@@ -1,14 +1,16 @@
 import json
 import time
+from typing import Optional
 
 from fastapi import WebSocket
 from fastapi import WebSocketDisconnect
 from fastapi.exceptions import WebSocketRequestValidationError
 
+import settings
 from distributedinference import api_logger
-from distributedinference.domain.user.entities import User
 from distributedinference.domain.node.entities import ConnectedNode
 from distributedinference.domain.node.entities import NodeMetrics
+from distributedinference.domain.user.entities import User
 from distributedinference.repository.node_repository import NodeRepository
 
 logger = api_logger.get()
@@ -17,10 +19,20 @@ logger = api_logger.get()
 async def execute(
     websocket: WebSocket,
     user: User,
+    model_name: Optional[str],
     node_repository: NodeRepository,
 ):
     logger.info(f"Node, with user id {user.uid}, trying to connect")
     await websocket.accept()
+
+    if not model_name:
+        raise WebSocketRequestValidationError('No "Model" header provided')
+    benchmark = await node_repository.get_node_benchmark(user.uid, model_name)
+    if not benchmark:
+        raise WebSocketRequestValidationError("Benchmarking is not completed")
+    if benchmark.tokens_per_second < settings.MINIMUM_COMPLETIONS_TOKENS_PER_SECOND:
+        raise WebSocketRequestValidationError("Benchmarking performance is too low")
+
     node_id = user.uid
     node_metrics = await node_repository.get_node_metrics(node_id) or NodeMetrics()
     node = ConnectedNode(
@@ -33,7 +45,9 @@ async def execute(
     logger.info(f"Node {node_id} connected")
     connect_time = time.time()
     if not node_repository.register_node(node):
-        raise WebSocketRequestValidationError("Node already connected")
+        raise WebSocketRequestValidationError(
+            "Node with same API key already connected"
+        )
     try:
         while True:
             data = await websocket.receive_text()
