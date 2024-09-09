5511307ed2dff8144f51cd73bef628b30e09223e
kristjanpeterson <kristjan@nftport.xyz>
2024-09-09 17:24:31 +0300

    revert one file

--- scripts/call_api.py
+++ scripts/call_api.py
@@ -6,16 +6,14 @@ import openai
 async def main():
     is_stream = True
     client = openai.AsyncOpenAI(
-        # base_url="http://localhost:5000/v1", api_key="asdasd-asd123"
-        base_url="http://34.78.190.171/v1",
-        api_key="mega-random-volvo-secret-user-0",
+        base_url="http://localhost:5000/v1", api_key="asdasd-asd123"
     )
     completion = await client.chat.completions.create(
         model="llama3",
         temperature=0,
         messages=[
             {"content": "You are a helpful assistant.", "role": "system"},
-            {"content": "respond with 1 word only", "role": "user"},
+            {"content": "Hello!", "role": "user"},
         ],
         stream=is_stream,
     )
