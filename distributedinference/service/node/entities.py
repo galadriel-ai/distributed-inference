b3c4361b63bffcc03fb49090343dfb4c8cc7f81f
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-09-12 14:13:25 +0300

    Sets cpu_model and cpu_count as non null

--- distributedinference/service/node/entities.py
+++ distributedinference/service/node/entities.py
@@ -11,8 +11,8 @@ from distributedinference.service.entities import ApiResponse
 class NodeInfoRequest(BaseModel):
     gpu_model: Optional[str] = Field(description="GPU model", default=None)
     vram: Optional[int] = Field(description="VRAM in MB", default=None)
-    cpu_model: Optional[str] = Field(description="CPU model", default=None)
-    cpu_count: int = Field(description="CPU cores count", default=0)
+    cpu_model: str = Field(description="CPU model")
+    cpu_count: int = Field(description="CPU cores count")
     ram: int = Field(description="RAM in MB")
     network_download_speed: float = Field(
         description="Network download speed in Mbps", default=None
