de186b694a93132b89bdcf93ec56a38771fd3cf4
Krešimir Grofelnik <kresimir.grofelnik@gmail.com>
2024-09-23 11:56:54 +0200

    Adds size check constraint on `user_profile#profile_data` (#68)
    * Adds size check constraint on `user_profile#profile_data`
    * Increases `profile_data` size limit to `2048`

---
+++ database/db_migrations/versions/000000000017_adds_profile_data_size_constraint.py
@@ -0,0 +1,30 @@
+"""Adds profile_data size constraint
+
+Revision ID: 000000000017
+Revises: 000000000016
+Create Date: 2024-09-18 14:47:37.666872
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "000000000017"
+down_revision = "000000000016"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # limit profile_data to 1024 bytes
+    op.create_check_constraint(
+        "profile_data_size_check_constraint",
+        "user_profile",
+        "octet_length(profile_data::text) <= 2048",
+    )
+
+
+def downgrade():
+    op.drop_constraint("profile_data_size_check_constraint", "user_profile")
