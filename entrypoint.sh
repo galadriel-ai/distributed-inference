a8b6bd679292d6c10caf9aeb2b1bf038468f91e7
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-09-26 14:08:10 +0200

    Decreases log level

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 #workers_to_spawn=$((cpu_units * 2 + 1))
 workers_to_spawn=1
 echo "Starting service with ${workers_to_spawn} workers"
-gunicorn --log-level debug --timeout 300 --bind 0.0.0.0:3000 --worker-class=uvicorn.workers.UvicornWorker --workers=$workers_to_spawn app:app
+gunicorn --log-level info --timeout 300 --bind 0.0.0.0:3000 --worker-class=uvicorn.workers.UvicornWorker --workers=$workers_to_spawn app:app
