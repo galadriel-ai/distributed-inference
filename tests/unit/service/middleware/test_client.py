45247497d1d85f5c3165ef7211ee8006e9bbffda
Maciej Witowski <maciej.witowski@protocol.ai>
2024-09-30 13:17:44 +0300

    Review comment

--- tests/unit/service/middleware/test_client.py
+++ tests/unit/service/middleware/test_client.py
@@ -1,16 +1,20 @@
 from distributedinference.service.middleware.client_version_validation_middleware import (
-    Client,
+    SupportedVersionRange,
 )
 
 
 async def test_client():
     test_cases = [
-        ("0.0.6", True),
-        ("0.0.9", True),
-        ("0.0.11", False),
+        (SupportedVersionRange("0.0.6", "0.0.9"), "0.0.5", False),
+        (SupportedVersionRange("0.0.6", "0.0.9"), "0.0.6", True),
+        (SupportedVersionRange("0.0.6", "0.0.9"), "0.0.9", True),
+        (SupportedVersionRange("0.0.6", "0.0.9"), "0.0.10", False),
+        (SupportedVersionRange("0.0.6", "0.0.10"), "0.0.10", True),
+        (SupportedVersionRange("0.0.6", "0.0.10"), "0.0.11", False),
+        (SupportedVersionRange("0.0.6", "0.1.0"), "0.0.11", True),
+        (SupportedVersionRange("0.1.2", "0.2.0"), "0.1.1", False),
+        (SupportedVersionRange("0.1.2", "0.2.0"), "0.1.2", True),
     ]
 
-    for version, expected_result in test_cases:
-        assert (
-            Client.GPU_NODE.is_version_supported(version) is expected_result
-        ), f"Version {version} support should be {expected_result}"
+    for range, version, expected_result in test_cases:
+        assert range.is_version_supported(version) == expected_result
