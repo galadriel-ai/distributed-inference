08df8c19ee43c078702066181f70d86b6728700e
kristjanpeterson <kristjan@nftport.xyz>
2024-10-07 13:42:25 +0300

    orjson.dumps in user repo to json.dumps to get str instead of bytes

--- distributedinference/service/auth/set_user_profile_data_service.py
+++ distributedinference/service/auth/set_user_profile_data_service.py
@@ -1,13 +1,14 @@
-from distributedinference.analytics.analytics import (
-    AnalyticsEvent,
-    EventName,
-    Analytics,
-)
+from distributedinference import api_logger
+from distributedinference.analytics.analytics import Analytics
+from distributedinference.analytics.analytics import AnalyticsEvent
+from distributedinference.analytics.analytics import EventName
+from distributedinference.domain.user.entities import User
 from distributedinference.repository.user_repository import UserRepository
 from distributedinference.service import error_responses
-from distributedinference.domain.user.entities import User
 from distributedinference.service.auth.entities import SetUserProfileDataRequest
 from distributedinference.service.auth.entities import SetUserProfileDataResponse
+
+logger = api_logger.get()
 
 
 async def execute(
@@ -21,5 +22,6 @@ async def execute(
         analytics.track_event(user.uid, AnalyticsEvent(EventName.SET_PROFILE_DATA, {}))
         analytics.identify_user(user, request.data)
     except:
+        logger.error(f"Error inserting user {user.uid} profile data", exc_info=True)
         raise error_responses.ValidationError()
     return SetUserProfileDataResponse()
