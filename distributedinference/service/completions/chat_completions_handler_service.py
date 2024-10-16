b7b48c7a25ff74d182d36e8ab29a5dc7b6c36419
Han <4738254+h4n0@users.noreply.github.com>
2024-10-16 15:19:36 +0200

    Formatting

--- distributedinference/service/completions/chat_completions_handler_service.py
+++ distributedinference/service/completions/chat_completions_handler_service.py
@@ -107,7 +107,9 @@ def _match_model_name(user_input: str) -> str:
     if model_name_base:
         # use 8b as default model if user doesn't specify model suffix
         suffix = components[1].lower() if len(components) > 1 else "8b"
-        exact_model_name = f"{model_name_base}-{settings.MODEL_NAME_SUFFIXES.get(suffix)}"
+        exact_model_name = (
+            f"{model_name_base}-{settings.MODEL_NAME_SUFFIXES.get(suffix)}"
+        )
         return exact_model_name
     else:
         # user may provide the full model name or wrong/unsupported model names
