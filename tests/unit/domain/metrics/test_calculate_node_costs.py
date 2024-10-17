90a666c7c0cf66dfbb04c2107974cf6dd5659d6b
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-10-17 13:07:49 +0200

    Node cost calculation + unit tests

--- tests/unit/domain/metrics/test_calculate_node_costs.py
+++ tests/unit/domain/metrics/test_calculate_node_costs.py
@@ -1,5 +1,7 @@
 from uuid import UUID
 
+from pytest import approx
+
 from distributedinference.domain.metrics import calculate_node_costs
 from distributedinference.domain.node.entities import NodeBenchmark
 
@@ -25,6 +27,21 @@ def test_one_node():
     assert result == {"model1": 0.4}
 
 
+def test_one_node_two_gpus():
+    result = calculate_node_costs.execute(
+        [
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model1",
+                benchmark_tokens_per_second=100,
+                gpu_model="NVIDIA GeForce RTX 4090",
+                gpu_count=2,
+            )
+        ]
+    )
+    assert result == {"model1": 0.8}
+
+
 def test_two_node():
     result = calculate_node_costs.execute(
         [
@@ -43,6 +60,28 @@ def test_two_node():
         ]
     )
     assert result == {"model1": 0.8}
+
+
+def test_two_node_with_3_gpus():
+    result = calculate_node_costs.execute(
+        [
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model1",
+                benchmark_tokens_per_second=100,
+                gpu_model="NVIDIA GeForce RTX 4090",
+                gpu_count=2,
+            ),
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model1",
+                benchmark_tokens_per_second=100,
+                gpu_model="NVIDIA GeForce RTX 4090",
+                gpu_count=1,
+            ),
+        ]
+    )
+    assert result == {"model1": approx(1.2)}
 
 
 def test_two_node_different_gpus():
@@ -65,6 +104,28 @@ def test_two_node_different_gpus():
     assert result == {"model1": 0.7}
 
 
+def test_two_node_different_multiple_gpus():
+    result = calculate_node_costs.execute(
+        [
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model1",
+                benchmark_tokens_per_second=100,
+                gpu_model="NVIDIA GeForce RTX 4090",
+                gpu_count=2,
+            ),
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model1",
+                benchmark_tokens_per_second=100,
+                gpu_model="NVIDIA GeForce RTX 4080",
+                gpu_count=1,
+            ),
+        ]
+    )
+    assert result == {"model1": approx(1.1)}
+
+
 def test_two_node_different_models():
     result = calculate_node_costs.execute(
         [
@@ -85,6 +146,28 @@ def test_two_node_different_models():
     assert result == {"model1": 0.4, "model2": 0.4}
 
 
+def test_two_node_different_models_multiple_gpus():
+    result = calculate_node_costs.execute(
+        [
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model1",
+                benchmark_tokens_per_second=100,
+                gpu_model="NVIDIA GeForce RTX 4090",
+                gpu_count=2,
+            ),
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model2",
+                benchmark_tokens_per_second=100,
+                gpu_model="NVIDIA GeForce RTX 4090",
+                gpu_count=1,
+            ),
+        ]
+    )
+    assert result == {"model1": 0.8, "model2": 0.4}
+
+
 def test_one_node_not_exact_match():
     result = calculate_node_costs.execute(
         [
@@ -97,3 +180,18 @@ def test_one_node_not_exact_match():
         ]
     )
     assert result == {"model1": 0.4}
+
+
+def test_one_node_not_exact_match_multiple_gpus():
+    result = calculate_node_costs.execute(
+        [
+            NodeBenchmark(
+                node_id=NODE_ID,
+                model_name="model1",
+                benchmark_tokens_per_second=100,
+                gpu_model="Some 4090 GPU",
+                gpu_count=3,
+            )
+        ]
+    )
+    assert result == {"model1": approx(1.2)}
