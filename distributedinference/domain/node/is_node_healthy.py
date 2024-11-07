49b65db797354cc4c62dcec7dfa597ffcd12d5c4
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-11-07 16:57:31 +0200

    Removes debug

--- distributedinference/domain/node/is_node_healthy.py
+++ distributedinference/domain/node/is_node_healthy.py
@@ -2,5 +2,4 @@ import settings
 
 
 def execute(time_to_first_token: float, throughput: float) -> bool:
-    print("is node healthy, time_to_first_token:", time_to_first_token, "throughput:", throughput)
     return time_to_first_token < settings.MIN_TIME_TO_FIRST_TOKEN_SEC and throughput > 0
