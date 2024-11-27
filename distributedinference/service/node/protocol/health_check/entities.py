6ba7d83df206b08e781a4b690f09a9115db74a0e
Maciej Witowski <maciej.witowski@protocol.ai>
2024-11-27 15:49:41 +0100

    Revert "Optional, for old nodes which don't send it"
    This reverts commit e780be2683bd8006820074578cb2b134cebfe698.

--- distributedinference/service/node/protocol/health_check/entities.py
+++ distributedinference/service/node/protocol/health_check/entities.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import List, Optional
+from typing import List
 
 from pydantic import BaseModel
 from pydantic import Field
@@ -25,7 +25,7 @@ class HealthCheckRequest(BaseModel):
 class HealthCheckGPUUtilization(BaseModel):
     gpu_percent: int = Field(description="GPU utilization, percent")
     vram_percent: int = Field(description="VRAM utilization, percent")
-    power_percent: Optional[int] = Field(description="Power utilization, percent", default=None)
+    power_percent: int = Field(description="Power utilization, percent")
 
 
 class HealthCheckResponse(BaseModel):
