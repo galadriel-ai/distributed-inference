dea4bdf3dcbec08cf6ba858ae35c461b13e9d5db
Han <windzhaohan@gmail.com>
2024-12-02 17:20:08 +0100

    Formatting & linting

--- distributedinference/utils/google_cloud_storage.py
+++ distributedinference/utils/google_cloud_storage.py
@@ -1,6 +1,6 @@
 import asyncio
 import base64
-from datetime import datetime
+from datetime import timedelta
 from google.cloud import storage
 from distributedinference import api_logger
 import settings
@@ -8,6 +8,7 @@ import settings
 logger = api_logger.get()
 
 URL_EXPIRATION_MINUTES = 2 * 60
+
 
 # pylint: disable=too-few-public-methods
 class GoogleCloudStorage:
@@ -21,14 +22,18 @@ class GoogleCloudStorage:
         else:
             self.client = None
 
-    async def decode_b64_and_upload_to_gcs(self, request_id: str, idx: int, image_b64: str) -> str:
+    async def decode_b64_and_upload_to_gcs(
+        self, request_id: str, idx: int, image_b64: str
+    ) -> str:
         if not self.client:
             # TODO probably save it locally and return the path?
             return image_b64
         image_bytes = base64.b64decode(image_b64)
         blob_name = f"{request_id}_{idx}.png"
         loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, self._upload_to_gcs, blob_name, image_bytes)
+        return await loop.run_in_executor(
+            None, self._upload_to_gcs, blob_name, image_bytes
+        )
 
     def _upload_to_gcs(self, blob_name: str, image_bytes: bytes) -> str:
         bucket = self.client.bucket(settings.GCS_BUCKET_NAME)
@@ -36,7 +41,7 @@ class GoogleCloudStorage:
         blob.upload_from_string(image_bytes, content_type="image/png")
         url = blob.generate_signed_url(
             version="v4",
-            expiration=datetime.timedelta(minutes=URL_EXPIRATION_MINUTES),
+            expiration=timedelta(minutes=URL_EXPIRATION_MINUTES),
             method="GET",
         )
         return url
